8fbb0c1787d6139be8eeed8c14a727a70b5d69fc
pemai <pemaina30@gmail.com>
2025-09-10 14:47:56 +0300

    Initial commit

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
-# Hey
+def calc(num1, num2, operation):
+    result = 0
+    # operation=('+','-','*','/')
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+       result = num1 / num2   
+    else:
+        print('Please enter a valid operation and number')
+
+    return result
+num1=float(input('Enter your first number '))
+num2=float(input('Enter your second number ')) 
+operation = input('Enter your preferred operation ')
+print(calc(num1, num2, operation))
